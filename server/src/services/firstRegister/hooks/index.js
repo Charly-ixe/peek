7080ac80fa64d82e747419a54cdd6865969dfa59
Benjamin Gammaire <benjamin.gammaire@gmail.com>
2017-04-24 19:27:33 +0200

    correct wording for email

--- server/src/services/firstRegister/hooks/index.js
+++ server/src/services/firstRegister/hooks/index.js
@@ -2,28 +2,31 @@
 
 const globalHooks = require('../../../hooks');
 const hooks = require('feathers-hooks');
+const EmailTemplate = require('email-templates').EmailTemplate
+const path = require('path')
 
-const test = function() {
-  console.log("test function");
-}
+const templateDir = path.join(__dirname, 'template')
+const newsletter = new EmailTemplate(templateDir)
 
 const sendRegistrationEmail = function () {
   return function (hook) {
       console.log("send email");
       console.log(hook.data.user_email);
-      var email = {
-         from: 'benjamin@peek.com',
-         to: hook.data.user_email,
-         subject: 'Retrouvez vos oeuvres en activant votre compte ! 🎨',
-         html: 'This is the email body'
-      };
+      newsletter.render(function (err, result) {
+        var email = {
+          from: 'hello@peek-expo.com',
+          to: hook.data.user_email,
+          subject: 'Expérience Peek 🖼  de la Bpi',
+          html: result.html
+        };
 
-      hook.app.service('mailer').create(email).then(function (result) {
-        console.log('Sent email', result);
-      }).catch(err => {
-        console.log(err);
-      });
-      return Promise.resolve(hook)
+        hook.app.service('mailer').create(email).then(function (result) {
+          console.log('Sent email', result);
+        }).catch(err => {
+          console.log(err);
+        });
+        return Promise.resolve(hook)
+      })
   }
 }
 
