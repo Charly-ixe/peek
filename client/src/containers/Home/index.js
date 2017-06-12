a8c817590727d46b1b87fdb206141152d3ab5bdd
Charly <j.gigonnet@gmail.com>
2017-06-12 10:58:35 +0200

    Work on Expos page

--- client/src/containers/Home/index.js
+++ client/src/containers/Home/index.js
@@ -49,6 +49,17 @@ export default Vue.extend({
 
   mounted () {
     this.createTls()
+    this.addListeners()
+
+    this.scroll = new Scroll({
+        preload: false,
+        native: false,
+        direction: 'horizontal',
+        section: document.querySelector('.home__expos-scroll-container'),
+        divs: document.querySelectorAll('.home__expo-container')
+    })
+
+    this.scroll.init()
 
   },
 
@@ -58,7 +69,18 @@ export default Vue.extend({
   methods: {
     createTls () {
       this.enterTl = new TimelineMax({paused: true})
+    },
+
+    addListeners() {
+      window.addEventListener('mousewheel', throttle(this.fadeOutInfos, 1200, {'trailing': false}))
+    },
+
+    fadeOutInfos() {
+      console.log("eghizehfgizfhl");
+      // Tweenmax.to(this.$refs.infoselt, 0.3, {opacity: 0, ease:Power1.easeInOut})
+
     }
+
   },
 
   components: {
